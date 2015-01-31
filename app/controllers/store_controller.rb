c8b73f754ae69b0b421300051babdc7dd6106b4a
bluefisher <blue_fisher@126.com>
2015-01-31 15:31:43 +0800

    depot ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,16 +1,28 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.find_products_for_sale
+  	@cart = find_cart
   end
   
   def add_to_cart
+	  logger.debug "debug----------begin"
   	product = Product.find(params[:id])
   	@cart = find_cart
   	@cart.add_product(product)
+  	#redirect_to_index nil
+	  logger.debug "debug----------end"
+  	
+  	respond_to do |format|
+  		format.html {redirect_to_index nil}
+  		format.js
+  	end
  	rescue ActiveRecord::RecordNotFound
  		logger.error("Attempt to access invalid product #{params[:id]}")
- 		redirect_to_index "Invalid product"
-
+ 		#redirect_to_index "Invalid product"
+ 		respond_to do |format|
+ 			format.html {redirect_to_index "Invalid product"}
+ 			format.js
+ 		end
   end
   
   def empty_cart
@@ -24,7 +36,7 @@ class StoreController < ApplicationController
   end
   
   def redirect_to_index(msg)
-  	flash[:notice] = msg
+  	flash[:notice] = msg if msg
   	redirect_to :action=>'index'
   end
 end
